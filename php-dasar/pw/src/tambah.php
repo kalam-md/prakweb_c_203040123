7741393dbe4b8a9ab4b5d228ab19e6a2266538b2
Kalam Mahardhika <kalammahardhika1002@gmail.com>
2022-10-03 10:58:10 +0700

    menambahkan animasi pada halaman tambah, halaman tambah fix

--- php-dasar/pw/src/tambah.php
+++ php-dasar/pw/src/tambah.php
@@ -70,7 +70,7 @@ if (isset($_POST['tambah'])) {
               <div>
                 <label class="block text-sm font-medium text-gray-700">Photo</label>
                 <div class="mt-1 flex items-center">
-                  <img src="./img/nophoto.jpg" width="220" style="display: block;" class="img-preview drop-shadow-xl rounded-xl border border-spacing-1">
+                  <img src="./img/nophoto.jpg" width="220" style="display: block;" class="img-preview drop-shadow-xl rounded-xl border border-spacing-1 transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-110 duration-300">
                   <input type="file" required name="gambar" class="gambar ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onchange="previewImage()">
                 </div>
               </div>
