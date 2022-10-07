52f70fe9c16070a7238abee7cd11cb8b9f4e94b2
Kalam Mahardhika <kalammahardhika1002@gmail.com>
2022-10-07 22:53:41 +0700

    mvc : video ke-enam -> Assets

---
+++ php-dasar/mvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_c_203040123/php-dasar/mvc/public');
--- php-dasar/mvc/app/views/about/index.php
+++ php-dasar/mvc/app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About me</h1>
-<p>halo saya <?= $data['nama']; ?>, umur <?= $data['umur']; ?> dan saya seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+  <h1>About me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.png" alt="kalam" width="200">
+  <p>halo saya <?= $data['nama']; ?>, umur <?= $data['umur']; ?> dan saya seorang <?= $data['pekerjaan']; ?></p>
+</div>
