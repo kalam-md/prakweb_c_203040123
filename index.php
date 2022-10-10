14ec6d7f21230cd31bb91af8b66805c866f4424f
Kalam Mahardhika <kalammahardhika1002@gmail.com>
2022-10-10 11:33:33 +0700

    index prakweb -> mengubah link tugas

--- index.php
+++ index.php
@@ -26,45 +26,43 @@
         <div class="bg-white px-4 py-5 sm:px-6">
           <p class="text-sm font-medium text-gray-500 mb-1">PHP Dasar</p>
           <div class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 grid grid-cols-2 gap-4">
-            <div class="divide-y divide-gray-200 rounded-md border border-gray-200 hover:bg-gray-100">
-              <div class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
-                <div class="flex w-0 flex-1 items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex-shrink-0 text-gray-400">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-                  </svg>
-                  <span class="ml-2 w-0 flex-1 truncate text-gray-500">Crud PHP</span>
-                </div>
-                <div class="ml-4 flex-shrink-0">
-                  <a href="./php-dasar/pw/src/index.php" class="font-medium text-indigo-600 hover:text-indigo-500">Click</a>
-                </div>
-              </div>
-            </div>
-            <div class="divide-y divide-gray-200 rounded-md border border-gray-200 hover:bg-gray-100">
-              <div class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
-                <div class="flex w-0 flex-1 items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex-shrink-0 text-gray-400">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-                  </svg>
-                  <span class="ml-2 w-0 flex-1 truncate text-gray-500">OOP PHP</span>
-                </div>
-                <div class="ml-4 flex-shrink-0">
-                  <a href="./php-dasar/oop/src/inheritance.php" class="font-medium text-indigo-600 hover:text-indigo-500">Click</a>
-                </div>
-              </div>
-            </div>
-            <div class="divide-y divide-gray-200 rounded-md border border-gray-200 hover:bg-gray-100">
-              <div class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
-                <div class="flex w-0 flex-1 items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex-shrink-0 text-gray-400">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-                  </svg>
-                  <span class="ml-2 w-0 flex-1 truncate text-gray-500">PHP MVC</span>
-                </div>
-                <div class="ml-4 flex-shrink-0">
-                  <a href="./php-dasar/mvc/public/" class="font-medium text-indigo-600 hover:text-indigo-500">Click</a>
+
+            <?php
+
+            $tugas = [
+              [
+                "nama" => "Crud PHP",
+                "link" => "./php-dasar/pw/src/index.php",
+              ],
+              [
+                "nama" => "OOP PHP",
+                "link" => "./php-dasar/oop/src/inheritance.php",
+              ],
+              [
+                "nama" => "PHP MVC",
+                "link" => "./php-dasar/mvc/public/",
+              ],
+            ];
+
+            ?>
+
+            <?php foreach ($tugas as $t) : ?>
+
+              <div class="divide-y divide-gray-200 rounded-md border border-gray-200 hover:bg-gray-100">
+                <div class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                  <div class="flex w-0 flex-1 items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex-shrink-0 text-gray-400">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                    </svg>
+                    <span class="ml-2 w-0 flex-1 truncate text-gray-500"><?= $t['nama']; ?></span>
+                  </div>
+                  <div class="ml-4 flex-shrink-0">
+                    <a href="<?= $t['link']; ?>" class="font-medium text-indigo-600 hover:text-indigo-500">Click</a>
+                  </div>
                 </div>
               </div>
-            </div>
+
+            <?php endforeach; ?>
           </div>
         </div>
       </dl>
